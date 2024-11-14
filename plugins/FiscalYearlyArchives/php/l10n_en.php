1f8bac9f9e9b2b76435cd7386535740e6169e8f3
Seiichiro Hazama <shazama@sixapart.com>
2024-11-14 18:06:16 +0900

    Added PHP English L10N. #4

---
+++ plugins/FiscalYearlyArchives/php/l10n_en.php
@@ -0,0 +1,14 @@
+<?php
+# Movable Type (r) (C) Six Apart Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
+global $Lexicon;
+$Lexicon = array(
+    'FISCAL-YEARLY_ADV' => 'Ficcal Yearly',
+    'CONTENTTYPE-FISCAL-YEARLY_ADV' => 'ContentType Ficcal Yearly',
+);
+
+?>
