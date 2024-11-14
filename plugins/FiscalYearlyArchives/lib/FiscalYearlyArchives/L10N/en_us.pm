11baeb8ac03eb572fc41e39515831527198a107e
Seiichiro Hazama <shazama@sixapart.com>
2024-11-14 17:15:51 +0900

    iAdded English L10N. #4

---
+++ plugins/FiscalYearlyArchives/lib/FiscalYearlyArchives/L10N/en_us.pm
@@ -0,0 +1,20 @@
+# Movable Type (r) (C) Six Apart Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
+package FiscalYearlyArchives::L10N::en_us;
+use strict;
+use warnings;
+use utf8;
+use MT::L10N;
+use vars qw( @ISA %Lexicon );
+@ISA = qw( MT::L10N );
+
+%Lexicon = (
+    'FISCAL-YEARLY_ADV' => 'Ficcal Yearly',
+    'CONTENTTYPE-FISCAL-YEARLY_ADV' => 'ContentType Ficcal Yearly',
+);
+
+1;
